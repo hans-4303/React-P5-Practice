646dc5f9ed2a5cebf4a60e576fe9e231f4fc2a40
definitiomea <103019360+definitiomea@users.noreply.github.com>
2023-02-27 16:50:18 +0900

    P5 이미지 카피 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import './App.css'
-import P5Index from './pages/P5Index'
+import React from "react";
+import "./App.css";
+import P5Index from "./pages/P5Index";
+import P5copy from "./pages/P5copy";
 
 function App() {
   return (
     <div className="App">
-      <P5Index></P5Index>
+      {/* <P5Index></P5Index> */}
+      <P5copy></P5copy>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/P5copy.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import Sketch from "react-p5";
+
+const P5copy = () => {
+  let img, newImg;
+
+  const preload = (p5) => {
+    /* 인스턴스에 이미지 부르기 */
+    img = p5.loadImage("../../img/ifJPbUm9XMsQdt7AQAets-1200-80.jpg");
+  };
+  const setup = (p5) => {
+    /* 캔버스 생성, 이미지의 너비는 유지, 높이 * 2 */
+    p5.createCanvas(img.width, img.height * 2);
+    /* newImg에 할당, 이미지 너비와 높이 만큼 */
+    newImg = p5.createImage(img.width, img.height);
+    /* 배경색 지정 */
+    p5.background(100);
+    /* 이미지 띄워보기 */
+    p5.image(img, 0, 0);
+    /* 이미지 카피 메서드 실행, p5 인수로 넘기기 */
+    imgCopy(p5);
+  }
+  /* p5는 파라미터로 받아오기 */
+  const imgCopy = (p5) => {
+    /* img에 loadPixels 메서드 호출 */
+    img.loadPixels();
+    /* 새 이미지에 loadPixels 메서드 호출 */
+    newImg.loadPixels();
+    /* 픽셀 호출을 알려고 함, [0]은 R, [1]은 G, [2]는 B, [3]은 알파 값을 말함 */
+    console.log(img.pixels[0]);
+    console.log(img.pixels[1]);
+    console.log(img.pixels[2]);
+    console.log(img.pixels[3]);
+    console.log(img.pixels);
+    for(let i = 0; i < img.width * img.height * 4; i += 4) {
+      newImg.pixels[i + 0] = img.pixels[i + 0];
+      newImg.pixels[i + 1] = img.pixels[i + 1];
+      newImg.pixels[i + 2] = img.pixels[i + 2];
+      newImg.pixels[i + 3] = img.pixels[i + 3];
+    }
+    newImg.updatePixels();
+    p5.image(newImg, 0, img.height);
+  }
+
+  return (
+    <div>
+      <Sketch preload={preload} setup={setup}></Sketch>
+    </div>
+  );
+};
+
+export default P5copy;
